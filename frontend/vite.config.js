58d63de9a14646087da6dfbe288e7175afd4c5c7
ValentinVulkov <valentinvalkov5210@gmail.com>
2026-03-14 16:01:33 +0200

    Connect ML model to frontend, added segments analysis UI

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+ 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
     server: {
-        proxy: {
-            "/predict": {
-                target: "http://127.0.0.1:8000",
-                changeOrigin: true,
-            },
-            "/health": {
-                target: "http://127.0.0.1:8000",
-                changeOrigin: true,
-            },
+    proxy: {
+        "/predict": {
+            target: "http://127.0.0.1:8000",
+            changeOrigin: true,
+        },
+        "/health": {
+            target: "http://127.0.0.1:8000",
+            changeOrigin: true,
+        },
+        "/artifacts": {
+            target: "http://127.0.0.1:8000",
+            changeOrigin: true,
         },
     },
+},
 })
+ 
